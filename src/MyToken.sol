d121ead7ff2347876d6ff3bb82c39316cd0cc4ef
Perelyn-sama <okohebina@gmail.com>
2022-05-30 06:35:43 +0100

    added mint and burn functions

--- src/MyToken.sol
+++ src/MyToken.sol
@@ -4,7 +4,19 @@ pragma solidity ^0.8.13;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract MyToken is ERC20 {
+    address public owner;
+
     constructor() ERC20("My Token", "MTKN") {
+        owner = msg.sender;
         _mint(msg.sender, 1000 * 10**decimals());
+    }
+
+    function mint(address account, uint256 amount) public {
+        require(msg.sender == owner, "Only Owner can mint");
+        _mint(account, amount);
+    }
+
+    function burn(address account, uint256 amount) public {
+        _burn(account, amount);
     }
 }
